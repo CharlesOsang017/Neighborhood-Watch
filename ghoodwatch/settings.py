8b66e26e136c7edf652dae87285265b556cea5bc
CharlesOsang017 <charlesosango02@gmail.com>
2020-11-02 11:39:02 +0300

    Add title to homepage

--- ghoodwatch/settings.py
+++ ghoodwatch/settings.py
@@ -148,8 +148,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 
 LOGIN_REDIRECT_URL = 'index'
